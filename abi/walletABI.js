5d2cd0d48e6323a2930bcdabe70dec123e1d2541
Akindu Hettiarachchi <akinduuwan123@gmail.com>
2025-11-24 13:09:44 +0530

    post deployment almost complete

---
+++ abi/walletABI.js
@@ -0,0 +1,299 @@
+const walletABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "initialOwner",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "ContractPaused",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "available",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "required",
+                "type": "uint256"
+            }
+        ],
+        "name": "InsufficientBalance",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "InvalidRecipient",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnableInvalidOwner",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "OwnableUnauthorizedAccount",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "TokenTransferFailed",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "recipient",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "TransferFailed",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "ZeroValue",
+        "type": "error"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "EthTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "Paused",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "TokenTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "Unpaused",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "fundme",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "pause",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "paused",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "sendContractEth",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "token",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "sendToken",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "unpause",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+module.exports = { walletABI };
--- test/postDeploy.test.js
+++ test/postDeploy.test.js
@@ -1,99 +1,130 @@
-require("dotenv").config();
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { JsonRpcProvider, Wallet, Contract } = require("ethers");
+const { myTokenABI } = require("../abi/myTokenABI");
+const { walletABI } = require("../abi/walletABI");
 
-describe("Post-deployment checks", function () {
-    let myToken;
-    let controlledWallet;
-    let owner;
-    let user1;
+async function deployFixture() {
+    const privateKey = process.env.PRIVATE_KEY;
+    const rpcProvider = process.env.SEPOLIA_RPC_URL;
+    const walletAddress = process.env.CONTROLLED_WALLET_ADDRESS;
+    const myTokenAddress = process.env.MYTOKEN_ADDRESS;
+    const initialSupply = 2000000;
 
-    before(async function () {
-        
-        const signers = await ethers.getSigners();
-        owner = signers[0]; 
+    const provider = new JsonRpcProvider(rpcProvider);
+    const signer = new Wallet(privateKey, provider);
 
-        user1 = signers[1];
+    const myToken = new Contract(myTokenAddress, myTokenABI, signer);
+    const controlledWallet = new Contract(walletAddress, walletABI, signer);
 
-        myToken = await ethers.getContractAt(
-            "MyToken",
-            process.env.MYTOKEN_ADDRESS
-        );
+    const decimals = await myToken.decimals();
+    const walletOwner = signer;
 
-        controlledWallet = await ethers.getContractAt(
-            "ControlledWallet",
-            process.env.CONTROLLED_WALLET_ADDRESS
-        );
-    });
+    return {
+        signer,
+        walletOwner,
+        walletAddress,
+        myTokenAddress,
+        initialSupply,
+        myToken,
+        controlledWallet,
+        decimals,
+    };
+}
 
-    it("token: has correct name, symbol and supply owner", async function () {
+describe("MyToken", function () {
+    it("has correct name and symbol", async function () {
+        const { myToken } = await deployFixture(deployFixture);
+
         expect(await myToken.name()).to.equal("Lion Coin");
         expect(await myToken.symbol()).to.equal("LEO");
-
-        const decimals = await myToken.decimals();
-        const totalSupply = await myToken.totalSupply();
-        const ownerBalance = await myToken.balanceOf(owner.address);
-
-        const expectedTotal = 2_000_000n * 10n ** BigInt(decimals);
-        expect(totalSupply).to.equal(expectedTotal);
-        expect(ownerBalance).to.equal(expectedTotal);
     });
+});
 
-    it("wallet: owner can pause/unpause", async function () {
-        const txPause = await controlledWallet.connect(owner).pause();
+
+describe("ControlledWallet - pause / unpause", function () {
+    it("owner can pause and unpause", async function () {
+        const { controlledWallet, walletOwner } = await deployFixture(deployFixture);
+
+        const txPause = await controlledWallet.connect(walletOwner).pause();
         await txPause.wait();
         expect(await controlledWallet.paused()).to.equal(true);
 
-        const txUnpause = await controlledWallet.connect(owner).unpause();
+        const txUnpause = await controlledWallet.connect(walletOwner).unpause();
         await txUnpause.wait();
         expect(await controlledWallet.paused()).to.equal(false);
     });
+});
 
-    it("wallet: sendContractEth works from deployed contract", async function () {
-        const walletAddress = await controlledWallet.getAddress();
-        const sendAmount = ethers.parseEther("0.05");
+describe("ControlledWallet - fundme", function () {
+    it("accepts eth and increases contract balance", async function () {
+        const { controlledWallet, signer } = await deployFixture(deployFixture);
 
-        const contractBalanceBefore = await ethers.provider.getBalance(walletAddress);
-        const userBalanceBefore = await ethers.provider.getBalance(user1.address);
+        const before = await ethers.provider.getBalance(controlledWallet);
+        const amount = ethers.parseEther("0.0001");
 
-        await expect(
-            controlledWallet.connect(owner).sendContractEth(user1.address, sendAmount)
-        ).to.emit(controlledWallet, "EthTransferred")
-         .withArgs(user1.address, sendAmount);
+        const tx = await controlledWallet.connect(signer).fundme({ value: amount });
+        await tx.wait();
 
-        const contractBalanceAfter = await ethers.provider.getBalance(walletAddress);
-        const userBalanceAfter = await ethers.provider.getBalance(user1.address);
-
-        expect(contractBalanceBefore - contractBalanceAfter).to.equal(sendAmount);
-        expect(userBalanceAfter).to.be.gt(userBalanceBefore);
-    });
-
-    it("wallet: sendToken works from deployed contract", async function () {
-        const decimals = await myToken.decimals();
-        const walletAddress = await controlledWallet.getAddress();
-
-        const tokenFundAmount = 100n * 10n ** BigInt(decimals);
-
-        // fund wallet with tokens if needed
-        const txFundToken = await myToken.connect(owner).transfer(walletAddress, tokenFundAmount);
-        await txFundToken.wait();
-
-        const sendAmount = tokenFundAmount / 2n;
-
-        const walletTokenBefore = await myToken.balanceOf(walletAddress);
-        const userTokenBefore = await myToken.balanceOf(user1.address);
-
-        await expect(
-            controlledWallet
-                .connect(owner)
-                .sendToken(await myToken.getAddress(), user1.address, sendAmount)
-        ).to.emit(controlledWallet, "TokenTransferred")
-         .withArgs(await myToken.getAddress(), user1.address, sendAmount);
-
-        const walletTokenAfter = await myToken.balanceOf(walletAddress);
-        const userTokenAfter = await myToken.balanceOf(user1.address);
-
-        expect(walletTokenBefore - walletTokenAfter).to.equal(sendAmount);
-        expect(userTokenAfter - userTokenBefore).to.equal(sendAmount);
+        const after = await ethers.provider.getBalance(controlledWallet);
+        expect(after - before).to.equal(amount);
     });
 });
+
+describe("ControlledWallet - withdraw", function () {
+    it("owner can withdraw eth from contract", async function () {
+        const { controlledWallet, signer, walletAddress, oneEth } = await deployFixture(deployFixture);
+
+        const withdrawAmount = ethers.parseEther("0.00001");
+        const contractBalanceBefore = await ethers.provider.getBalance(walletAddress);
+
+        const tx = await controlledWallet.connect(signer).withdraw(withdrawAmount);
+        await tx.wait();
+
+        const contractBalanceAfter = await ethers.provider.getBalance(walletAddress);
+        expect(contractBalanceBefore - contractBalanceAfter).to.equal(withdrawAmount);
+    });
+
+});
+
+// describe("ControlledWallet - sendContractEth", function () {
+//     it("sends eth from contract to recipient and emits event", async function () {
+//         const { controlledWallet, walletOwner, walletAddress, user1 } = await loadFixture(deployFixture);
+
+//         const sendAmount = ethers.parseEther("0.25");
+
+//         const contractBalanceBefore = await ethers.provider.getBalance(walletAddress);
+//         const userBalanceBefore = await ethers.provider.getBalance(user1.address);
+
+//         await expect(controlledWallet.connect(walletOwner).sendContractEth(user1.address, sendAmount)).to.emit(controlledWallet, "EthTransferred").withArgs(user1.address, sendAmount);
+
+//         const contractBalanceAfter = await ethers.provider.getBalance(walletAddress);
+//         const userBalanceAfter = await ethers.provider.getBalance(user1.address);
+
+//         expect(contractBalanceBefore - contractBalanceAfter).to.equal(sendAmount);
+//         expect(userBalanceAfter).to.be.gt(userBalanceBefore);
+//     });
+// });
+
+// describe("ControlledWallet - sendToken", function () {
+//     it("sends erc20 tokens from wallet to recipient and emits event", async function () {
+//         const { controlledWallet, myToken, walletOwner, user1, walletAddress, decimals } = await loadFixture(deployFixture);
+//         const tokenFundAmount = 100n * 10n ** BigInt(decimals);
+//         const approvefortransfer = await myToken.connect(walletOwner).approve(walletOwner, tokenFundAmount);
+//         await approvefortransfer.wait();
+//         const txFundToken = await myToken.connect(walletOwner).transfer(walletAddress, tokenFundAmount);
+//         await txFundToken.wait();
+//         const sendAmount = tokenFundAmount / 2n;
+//         const walletTokenBefore = await myToken.balanceOf(walletAddress);
+//         const userTokenBefore = await myToken.balanceOf(user1.address);
+
+//         await expect(controlledWallet.connect(walletOwner).sendToken(await myToken.getAddress(), user1.address, sendAmount)).to.emit(controlledWallet, "TokenTransferred").withArgs(await myToken.getAddress(), user1.address, sendAmount);
+
+//         const walletTokenAfter = await myToken.balanceOf(walletAddress);
+//         const userTokenAfter = await myToken.balanceOf(user1.address);
+
+//         expect(walletTokenBefore - walletTokenAfter).to.equal(sendAmount);
+//         expect(userTokenAfter - userTokenBefore).to.equal(sendAmount);
+//     });
+
+// });
